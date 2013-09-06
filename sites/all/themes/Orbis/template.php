c02c4c94e92f55ec64c453dea9cfbb6630463640
David Mares <mares@diib-pc.(none)>
2013-09-05 21:29:28 -0500

    modificaicon del tema Orbis

--- sites/all/themes/Orbis/template.php
+++ sites/all/themes/Orbis/template.php
@@ -3,3 +3,29 @@
 /**
  * @file template.php
  */
+function Orbis_theme(&$existing, $type, $theme, $path){
+	$items = array();
+
+	$items['user_login'] = array(
+		'render element' => 'form',
+		'path' => drupal_get_path('theme','Orbis') . '/templates',
+		'template' => 'user-login',
+		'preprocess functions' => array(
+			'Orbis_preprocess_user_login'
+		)
+	);
+
+	$items['user_login_block'] = array(
+		'template' => 'templates/user-login-block',
+		'render element' => 'form',
+	);
+
+	return $items;
+}
+
+function Orbis_preprocess_user_login_block(&$vars) {
+  $vars['name'] = render($vars['form']['name']);
+  $vars['pass'] = render($vars['form']['pass']);
+  $vars['submit'] = render($vars['form']['actions']['submit']);
+  $vars['rendered'] = drupal_render_children($vars['form']);
+}
---
+++ sites/all/themes/Orbis/templates/user-login-block.tpl.php
@@ -0,0 +1,8 @@
+<div id="user-login-block">
+    <?php print $name; // Display username field ?>
+    <?php print $pass; // Display Password field ?>
+    <?php print $submit; // Display submit button ?>
+  <div class="links">
+    <?php print $rendered; // Display hidden elements (required for successful login) ?> 
+  </div>
+</div>
